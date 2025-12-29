6ae158d8be4a8859e1ca3d25f1f5acc21439010f
Joshua Mathews <joshfeltonm@gmail.com>
2025-12-29 08:50:04 -0700

    Route planning + formatting

--- homepage.ml
+++ homepage.ml
@@ -1,3 +1,16 @@
-let () =
-  Dream.run (fun _ ->
-    Dream.html "Good morning, world!")
+let routes =
+  [
+    Dream.get "/" (fun _ -> Dream.html "hello");
+    Dream.get "/blog" (fun _ -> Dream.html "list of blog post previews");
+    (* Dream.post "/blog" *)
+    Dream.get "/blog/:post_id" (fun request ->
+        Dream.html (Dream.param request "post_id"));
+    (* Dream.put "/blog/:post_id" *)
+    (* Dream.post "/blog/:post_id/comments" *)
+    (* Dream.put "/blog/:post_id/comments/:comment_id" *)
+    (* Dream.get "/login" *)
+    (* Dream.post "/login" *)
+    (* Dream.delete "/logout" *)
+  ]
+
+let () = Dream.run @@ Dream.logger @@ Dream.router routes

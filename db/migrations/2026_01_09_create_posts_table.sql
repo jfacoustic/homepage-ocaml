d3c3125b6e03ea4fd556e67eb1d69a18eb30392b
Joshua Mathews <joshfeltonm@gmail.com>
2026-01-12 18:28:58 -0700

    Delete unnecessary files

--- db/migrations/2026_01_09_create_posts_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [IF NOT EXISTS] blog_posts (
-  blog_post_id INTEGER PRIMARY KEY,
-  title TEXT NOT NULL,
-  content TEXT NOT NULL,
-  created_at TEXT NOT NULL DEFAULT current_timestamp,
-  updated_at TEXT NOT NULL DEFAULT current_timestamp
-);
